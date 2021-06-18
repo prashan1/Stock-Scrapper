f2e69394ba7d405c1cf7de84d7b17d936a2dc2d7
Prashant Bisht <57286404+prashan1@users.noreply.github.com>
2021-06-18 10:14:49 +0530

    Update crawller.py

--- scrapper/scrapper/spiders/crawller.py
+++ scrapper/scrapper/spiders/crawller.py
@@ -22,10 +22,21 @@ class CrawllerSpider(scrapy.Spider):
                 "PEtotOI" : data['filtered']['PE']['totOI'],
                 "PEtotVoi" : data['filtered']['PE']['totVol'],
                 }
-            newD.update({'strikePrice':data['filtered']['data'][i]['CE']['strikePrice']})
-            newD.update({ f'ce{valid}': data['filtered']['data'][i]['CE'][valid]    for valid in self.validAtributes  })
-            newD.update({ f'pe{valid}': data['filtered']['data'][i]['PE'][valid]    for valid in self.validAtributes  })
+            # Checking if CE is absent in data 
+            try:
+                newD.update({ f'ce{valid}': data['filtered']['data'][i]['CE'][valid]    for valid in self.validAtributes  })
+                newD.update({'strikePrice':data['filtered']['data'][i]['CE']['strikePrice']})
+            except Exception as e : 
+                newD.update({ f'ce{valid}': 0.00    for valid in self.validAtributes  })
+
+            # Checking if PE is absent in data
+
+            try:
+                newD.update({'strikePrice':data['filtered']['data'][i]['PE']['strikePrice']})
+                newD.update({ f'pe{valid}': data['filtered']['data'][i]['PE'][valid]    for valid in self.validAtributes  })
+            except Exception as e : 
+                newD.update({ f'pe{valid}': 0.00    for valid in self.validAtributes  })
+
             for dataitem in newD:
                 marketObj[dataitem] = newD[dataitem]  
             yield marketObj 
-        
